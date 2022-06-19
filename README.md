e2b95a601d308cf295f83dd987fd790c22ff2322
谦哥 <qqyule@gmail.com>
2022-06-19 13:45:38 +0800

    ✨ feat(增加monggodb):

---
+++ README.md
@@ -0,0 +1,4 @@
+# 构建镜像
+docker-compose build
+# 运行5个相同 --scale node=5
+docker-compose up -d --scale node=5
--- node/app.js
+++ node/app.js
@@ -1,12 +1,20 @@
-let express = require('express')
-let os = require('os')
-let app = express()
+const express = require('express')
+const os = require('os')
+const app = express()
+const mongoose = require('mongoose')
+// mongodb链接地址
+const DB_URL = 'mongodb://root:mongo123@localhost:27017/user?authSource=admin'
+// 连接数据库
+mongoose.connect(DB_URL)
+mongoose.connection.on('connect', () => {
+    console.log('mongodb connect success')
+})
 // 获取本机ip地址
-function getLocalIpAddress () {
+function getLocalIpAddress() {
     let ip = ''
     let netInfo = os.networkInterfaces()
     let osType = os.type()
-    if (osType === 'Windows_NT') { 
+    if (osType === 'Windows_NT') {
         for (const dev in netInfo) {
             // win7的网络信息中显示为本地连接，win10显示为以太网
             if (dev === '本地连接' || dev === '以太网') {
@@ -28,6 +36,44 @@ app.get('/getJson', (request, response) => {
         title: 'Hello Express、Hello Docker',
         ip: getLocalIpAddress(),
         env: process.env.NODE_ENV
+    })
+})
+
+
+//定义User模型
+const User = mongoose.model('user', new mongoose.Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: Number,
+        require: true
+    }
+}))
+app.get('/getUser', (request, response) => {
+    User.find({}, (err, doc) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(doc)
+            response.json(doc)
+        }
+
+    })
+})
+app.get('/addUser', (request, response) => {
+    User.create({
+        username: 'laowang',
+        age: 12
+    }, (err, doc) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(doc)
+            response.json(doc)
+        }
+
     })
 })
 // 监听3000端口
